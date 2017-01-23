ecd617206610f5e6e07eb4078298edaf902e7df4
MottoX <micrari@gmail.com>
2017-01-24 01:13:08 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mysql-replication
-Personal docker image with mysql replication set
+
+The image is based on official [mysql-server](https://hub.docker.com/r/mysql/mysql-server/) with replication set.
--- replication-entrypoint.sh
+++ replication-entrypoint.sh
@@ -3,13 +3,10 @@ set -eo pipefail
 
 MYSQL_CNF_FILE="/etc/my.cnf"
 
-
-
 if [ -n "$MASTER_PORT_3306_TCP_ADDR" ]; then
     export MASTER_HOST=$MASTER_PORT_3306_TCP_ADDR
     export MASTER_PORT=$MASTER_PORT_3306_TCP_PORT
 fi
-
 
 if [ $REPLICATION_ROLE = "master" ]; then
     cat > ${MYSQL_CNF_FILE}  << 'EOF'
@@ -32,7 +29,6 @@ if [ $REPLICATION_ROLE = "master" ]; then
 
     log-error=/var/log/mysqld.log
     pid-file=/var/run/mysqld/mysqld.pid
-
 EOF
 elif [ $REPLICATION_ROLE = "slave" ]; then
     cat > ${MYSQL_CNF_FILE}  << 'EOF'
